3d6dbef5ea4c47f95a3638abb2a458bca7a1e6fd
suncong@lianjia.com <386866711@qq.com>
2015-11-18 19:05:31 +0800

    homework6

--- homework6/homework6.java
+++ homework6/homework6.java
@@ -8,52 +8,60 @@ import java.util.Map.Entry;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		File f1 = new File("1.txt");
-		File f2 = new File("2.txt");
-		
-		BufferedReader br1 = new BufferedReader(new FileReader(f1));
-		BufferedReader br2 = new BufferedReader(new FileReader(f2));
 		HashMap<String,Integer> map = new HashMap<String,Integer>();
+		File file1 = new File("sun.txt");
+		File file2 = new File("cong.txt");
+		BufferedReader bufr1 = new BufferedReader(new FileReader(file1));
+		BufferedReader bufr2 = new BufferedReader(new FileReader(file2));		
+		int count1 = 0,count2 = 0,count3=0;
+		float perc1,perc2;
 		String line;
-		int count1 = 0,count2 = 0;
-		while((line= br1.readLine()) != null) {
+		while((line= bufr1.readLine()) != null) {
 			String str[] = line.split("\\W+");
-			for (String s : str) {
-				if(!map.containsKey(s)) {
-					map.put(s, 1);
+			for (String string : str) {
+				if(!map.containsKey(string)) {
+					map.put(string, 1);
 				}
 				count1 ++;
 			}
 			
 		}
-		while((line= br2.readLine()) != null) {
+		while((line= bufr2.readLine()) != null) {
 			String str[] = line.split("\\W+");
-			for(String s :str) {
-				if(!map.containsKey(s)) {
-					map.put(s, 1);
+			for(String string :str) {
+				if(!map.containsKey(string)) {
+					map.put(string, 1);
 				}else {
-					map.put(s, 2);
+					map.put(string, map.get(string) + 1);
 				}
 				count2++;
 			}
 			
 		}
-		System.out.print("两个文件总共的词汇表有：");
+		System.out.print("两个文件中总共的词汇表有：");
 		for(String key : map.keySet()) {
 			System.out.print(key + " ");
 		}
 		System.out.println();
-		System.out.print("同时出现在两个文件中的交集单词词汇表有：");
+		System.out.print("两个文件中都出现的单词词汇表有：");
 		Iterator<Entry<String, Integer>> it = map.entrySet().iterator();  
 		while (it.hasNext()) {  
 		    Map.Entry<String, Integer> entry = it.next(); 
-		    if(entry.getValue() == 2) {
-		    	System.out.print(entry.getKey() + " ");  
+		    if(entry.getValue() != 1) {
+		    	System.out.print(entry.getKey() + " "); 
+		    	count3++;
 		    }
 		}  
+		
 		System.out.println();
-		System.out.print("两个文件词汇表中各自包含单词总数分别为：" + count1 + "," + count2);
+		perc1 = ComputePercent(count3,count1);
+		perc2 = ComputePercent(count3,count2);
+		System.out.print("两个文件词汇表的交集词汇占各文件单词总数分别为：" + perc1 * 100 + "%" + "," + perc2 * 100 + "%");
 		System.out.println();
 		
+	}
+	public static float ComputePercent(int a ,int b) {
+		float result = (float)a/b;
+		return result;
 	}
 }
